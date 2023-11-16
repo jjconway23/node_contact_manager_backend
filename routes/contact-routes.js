30090a94b2c5e6d770e44a11d22b8841c5f3651a
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-16 16:59:44 +0000

    Add basic routes for get post put and delete

--- routes/contact-routes.js
+++ routes/contact-routes.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const router = express.Router();
 
-router.route('/', (req, res)=> {
-    res.send("Contacts found all")
-})
-
+router.route('/') 
+    .get((req,res)=> {
+        res.send('Get all Contacts')
+    })
+router.route('/') 
+    .post((req,res)=> {
+        res.send('Get all Contacts')
+    })
+router.route('/:id') 
+    .get((req,res)=> {
+        res.send('Get all Contacts')
+    })
+router.route('/:id') 
+    .put((req,res)=> {
+        res.send('Get all Contacts')
+    })
+router.route('/:id') 
+    .delete((req,res)=> {
+        res.send('Get all Contacts')
+    })
 module.exports = router
+
+
+
+// .put(':id', (req, res) => {
+//     res.send("Update Contact for " + req.params.id)
+// })
+// .delete(':id', (req, res) => {
+//     res.send("Delete Contact for " + req.params.id)
+// })
