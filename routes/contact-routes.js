3966f9756e5a8b88d7e4d26e0507ae692bd7d62a
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-16 17:14:22 +0000

    Refactor contacts route some more

--- routes/contact-routes.js
+++ routes/contact-routes.js
@@ -1,27 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const {getContacts, getContact, createContact, updateContact, deleteContact} = require('../controllers/contact-controller')
-router.route('/') 
-    .get(getContacts)
 
 router.route('/') 
+    .get(getContacts)
     .post(createContact)
 
 router.route('/:id') 
     .get(getContact)
-
-router.route('/:id') 
     .put(updateContact)
-
-router.route('/:id') 
     .delete(deleteContact)
+
 module.exports = router
-
-
-
-// .put(':id', (req, res) => {
-//     res.send("Update Contact for " + req.params.id)
-// })
-// .delete(':id', (req, res) => {
-//     res.send("Delete Contact for " + req.params.id)
-// })
