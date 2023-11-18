bb64bd8c08f938f6749a38f48446bfdcad3dd75a
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-18 14:17:24 +0000

    add basic logic to the error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,15 @@
 const errorHandler = (err,req,res,next)=> {
-
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.json({
+        title: "Not Found",
+        message:err.message, 
+        stackTrace: err.stack
+    })
+    res.json({
+        title: "Validation Failed",
+        message:err.message, 
+        stackTrace: err.stack
+    })
 }
 
 module.exports = errorHandler
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const router = require('./routes/contact-routes')
+const errorHandler = require('./middleware/errorhandler')
 const app = express()
 
 
 app.use(express.json())
 app.use('/api/contacts', router)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5007
